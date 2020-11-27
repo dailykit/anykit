9c113adaa33a5989fbcab4d2ee7e7013343e68a3
siddhant <siddhantk232@gmail.com>
2020-11-27 19:12:04 +0530

    feat: renderBtn()
    render a hero btn onInit of this script.

--- src/anyKit.ts
+++ src/anyKit.ts
@@ -1,9 +1,14 @@
+import { log } from "./utils/logger";
+
 type options = {
   type: "manual" | "auto";
   ingredients: string;
   steps: string;
   inventory: string;
-  btnLocation: string;
+  button: {
+    location: string;
+    text?: string;
+  };
 };
 
 export class AnyKit {
@@ -12,5 +17,32 @@ export class AnyKit {
     this.config = config;
   }
 
-  init() {}
+  showModal(e: MouseEvent) {
+    console.log("show modal fn called", e);
+  }
+
+  renderBtn() {
+    const btn = document.createElement("button");
+    const target = document.querySelector(this.config.button.location);
+
+    if (!target) {
+      log(
+        "error",
+        new Error(
+          `no element found on this page with target=${this.config.button.location}`
+        )
+      );
+      return;
+    }
+
+    btn.innerHTML = this.config.button.text || "Buy as MealKit";
+    btn.addEventListener("click", this.showModal);
+    btn.classList.add("anykit__btn-hero");
+    target?.appendChild(btn);
+  }
+
+  init() {
+    // render "buy me a mealkit button"
+    this.renderBtn();
+  }
 }
