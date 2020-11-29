d4857c323fb5fc41658f17e3894adb9742b5d136
siddhant <siddhantk232@gmail.com>
2020-11-29 21:03:46 +0530

    feat: logger prefixex
    prefixes to make the anykit logger standout in the console

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,18 @@
 export const log = (type: "info" | "error", ...messages: any) => {
   switch (type) {
     case "info":
-      for (const infoMsg of messages) console.log(infoMsg);
+      for (const infoMsg of messages) _log(type, infoMsg);
       break;
     case "error":
-      for (const errMsg of messages) console.error(errMsg);
+      for (const errMsg of messages) _error(type, errMsg);
       break;
     default:
-      for (const msg of messages) console.log(msg);
+      for (const msg of messages) _log(type, msg);
   }
 };
+
+const _log = (type: "info" | string, message: any) =>
+  console.log(`[ANYKIT][${type.toUpperCase()}]: ${message}`);
+
+const _error = (type: "error" | string, message: any) =>
+  console.error(`[ANYKIT][${type.toUpperCase()}]: ${message}`);
