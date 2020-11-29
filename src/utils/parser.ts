5eafe9f025dc63880be050c72277f8765407369c
siddhant <siddhantk232@gmail.com>
2020-11-29 14:21:42 +0530

    feat: cache query result
    cache the dom query for json+ld scripts

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,10 +1,14 @@
 import { Recipe } from "schema-dts";
 import { log } from "./logger";
 
+let recipe: Recipe;
+
 export const parseJsonld = (): Recipe | undefined => {
   // read more about "Recipe" schema here https://schema.org/Recipe
   // get all json+ld script tags and filter out every schema except "Recipe"
   try {
+    if (recipe) return recipe;
+
     const sources = Array.from(
       document.querySelectorAll('script[type="application/ld+json"]')
     )
@@ -12,7 +16,7 @@ export const parseJsonld = (): Recipe | undefined => {
       .filter((s) => s["@type"] === "Recipe");
 
     // At this point, the script expects only one schema for "Recipe". The first schema in sources will be used
-
+    recipe = sources[0];
     return sources[0];
   } catch (error) {
     log("error", error);
