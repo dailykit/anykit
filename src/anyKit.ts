119f564075b6731f673fb37fa38556d0673c230d
siddhant <siddhantk232@gmail.com>
2020-12-07 13:45:37 +0530

    change: sending recipeUrl to the server

--- src/anyKit.ts
+++ src/anyKit.ts
@@ -1,11 +1,6 @@
 import { log } from "./utils/logger";
-import { parseJsonld, parseMicrodata } from "./parser";
 
 type options = {
-  type: "manual" | "auto";
-  ingredients: string;
-  steps: string;
-  inventory: string;
   button: {
     location: string;
     text?: string;
@@ -16,26 +11,26 @@ export class AnyKit {
   config: options;
   constructor(config: options) {
     this.config = config;
-    let schema = parseJsonld();
-    if (!schema) {
-      log(
-        "info",
-        "Recipe schema not available in json+ld format",
-        "trying microdata format"
-      );
-      schema = parseMicrodata();
-    }
 
-    if (!schema) {
-      log(
-        "info",
-        "No recipe schema found",
-        "read more about Recipe schema here https://schema.org/Recipe"
-      );
-      return;
-    }
+    // send page url to anykit-server
+    const { location } = window;
 
-    log("info", schema);
+    fetch("http://localhost:5000/recipes", {
+      method: "POST",
+      body: JSON.stringify({
+        recipeUrl: location.toString(),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((req) => {
+        log("info", req.status);
+        return req.json();
+      })
+      .then((res) => {
+        log("info", res);
+      });
   }
 
   init() {
