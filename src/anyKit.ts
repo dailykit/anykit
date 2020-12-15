4f9fe2940be5dd4eb68c1d5097ceeabcf2c14e81
siddhant <siddhantk232@gmail.com>
2020-12-15 19:29:34 +0530

    feat: append BMK btn in body if !location
    appends the BMK btn in body if `this.config.button.location`
    is undefined
    else appends the btn in the specified location

--- src/anyKit.ts
+++ src/anyKit.ts
@@ -2,7 +2,7 @@ import { log } from "./utils/logger";
 
 type options = {
   button: {
-    location: string;
+    location?: string;
     text?: string;
   };
 };
@@ -36,20 +36,26 @@ export class AnyKit {
   init() {
     // render "buy me a mealkit button"
     const btn = document.createElement("button");
-    const target = document.querySelector(this.config.button.location);
-
-    if (!target) {
-      log(
-        "error",
-        new Error(
-          `no element found on this page with target=${this.config.button.location}`
-        )
-      );
-      return;
-    }
-
     btn.innerHTML = this.config.button.text || "Buy as MealKit";
     btn.classList.add("anykit__btn-hero");
-    target?.appendChild(btn);
+
+    if (this.config.button.location) {
+      const target = document.querySelector(this.config.button.location);
+
+      if (!target) {
+        log(
+          "error",
+          new Error(
+            `no element found on this page with target=${this.config.button.location}`
+          )
+        );
+        return;
+      }
+
+      target?.appendChild(btn);
+    } else {
+      btn.classList.add("anykit__btn-fixed");
+      document.body.appendChild<HTMLButtonElement>(btn);
+    }
   }
 }
