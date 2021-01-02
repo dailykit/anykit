f91efa9b6ea59a79487d34f4531492936f6c52d2
siddhantk232 <siddhantk232@gmail.com>
2021-01-02 20:20:10 +0530

    feat: terser setup in prod
    use rollup-plugin-terser in prod

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,18 @@
 const pkg = require("./package.json");
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const { terser } = require("rollup-plugin-terser");
 const filesize = require("rollup-plugin-filesize");
 
 // add terser
-export default [
-  {
-    input: "build/index.js",
-    output: {
-      file: pkg["module"],
-      format: "esm",
-    },
-    plugins: [nodeResolve(), filesize()],
+const config = {
+  input: "build/index.js",
+  output: {
+    file: pkg["module"],
+    format: "esm",
   },
-];
+  plugins: [nodeResolve(), filesize()],
+};
+
+if (process.env.NODE_ENV === "production") config.plugins.push(terser());
+
+module.exports = exports = config;
