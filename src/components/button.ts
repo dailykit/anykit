72213b07a41fa85da3c6163af7bb3d057bc7acbc
siddhantk232 <siddhantk232@gmail.com>
2021-01-02 20:00:16 +0530

    feat: using mwc web components
    using mwc button and dialog components
    for UI
    wrapped as anykit-app customElement

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,7 +1,11 @@
 import { render, html } from "lit-html";
+import "@material/mwc-button";
+import "@material/mwc-dialog";
+import { log } from "../utils/logger";
 
-class AnykitButton extends HTMLElement {
+export class AnykitApp extends HTMLElement {
   _text: string = "";
+  _modalOpened: boolean = false;
 
   constructor() {
     super();
@@ -22,35 +26,57 @@ class AnykitButton extends HTMLElement {
     this.update();
   }
 
+  get modalOpened() {
+    return this._modalOpened;
+  }
+
+  set modalOpened(value: boolean) {
+    this._modalOpened = value;
+    this.update();
+  }
+
+  toggleModal = () => (this.modalOpened = !this.modalOpened);
+  closeModal = () => (this.modalOpened = false);
+
   update() {
+    log("info", this.modalOpened, this.text);
     render(this.template(), this.shadowRoot as any, { eventContext: this });
   }
 
   template() {
     return html` <style>
-        button {
-          background-color: tomato;
-          color: white;
-          padding: 1.25rem;
-          border: 0;
-          border-radius: 0.25rem;
-          cursor: pointer;
-        }
-
-        .fixed {
-          position: fixed;
-          right: 1rem;
-          bottom: 1rem;
-          z-index: 10000;
-        }
-
-        button:hover {
-          filter: brightness(0.9);
+        mwc-button {
+          --mdc-theme-primary: #ff9e2a;
+          --mdc-theme-on-primary: white;
+          ${window.anykitConfig?.isManual
+          ? html`position: fixed; right: 1rem; bottom: 1rem; z-index: 10000`
+          : html``}
         }
       </style>
+      <mwc-dialog
+        heading="Hello Anykit"
+        @closed=${this.closeModal}
+        ?open=${this.modalOpened}
+      >
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lectus
+          tortor, viverra et erat eget, sodales mattis massa. Fusce consectetur
+          tempus eros, non hendrerit urna vestibulum vel. Maecenas fringilla
+          lorem non libero aliquam scelerisque. Quisque egestas magna nec metus
+          semper bibendum. Aliquam at posuere nunc, quis euismod enim. Morbi et
+          enim in lectus molestie ultricies in in tellus. Pellentesque egestas
+          in dui ut facilisis. Curabitur dapibus malesuada dui vitae interdum.
+          Sed et sagittis enim. In eleifend tincidunt tellus vel pulvinar. Cras
+        </div>
+        <mwc-button slot="secondaryAction" dialogAction="cancel">
+          cancel
+        </mwc-button>
+      </mwc-dialog>
 
-      <button>${this.text}</button>`;
+      <mwc-button
+        @click=${this.toggleModal}
+        unelevated
+        label="${this.text}"
+      ></mwc-button>`;
   }
 }
-
-customElements.define("anykit-button", AnykitButton);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import "./components/button";
 import "./anyKit";
+import "./components/button";
